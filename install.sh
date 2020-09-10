76fbe54d7b81b735036447af3c36d0a7b06b5431
fuckup <31011703+fuckup1337@users.noreply.github.com>
2020-09-10 21:04:33 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 sudo apt-get install python3-pip
 sudo apt-get install mono-complete
 sudo pip3 install -e .
-git clone https://github.com/Accenture/CLRvoyance EXCELntDonut/CLRvoyance
+git clone https://github.com/Accenture/CLRvoyance EXCELntDonut/CLRvoyance && mv EXCELntDonut/CLRvoyance/CLRvoyance/* EXCELntDonut/CLRvoyance
