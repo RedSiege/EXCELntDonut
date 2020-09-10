8e9544210233d2d549fc3c29cab86851c38ae20d
fuckup <31011703+fuckup1337@users.noreply.github.com>
2020-09-10 21:02:43 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,4 @@
-if [ "$(id -u)" != "0" ]; then
-  echo '[Error]: You must run this setup script with root privileges.'
-  echo
-  exit 1
-fi
-apt-get install python3-pip
-apt-get install mono-complete
-pip3 install -e .
+sudo apt-get install python3-pip
+sudo apt-get install mono-complete
+sudo pip3 install -e .
+git clone https://github.com/Accenture/CLRvoyance EXCELntDonut/CLRvoyance
