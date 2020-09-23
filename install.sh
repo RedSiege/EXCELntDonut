e2b3a8df61d2be2e243a5627c46668aec567d99b
joeleonjr <20135619+joeleonjr@users.noreply.github.com>
2020-09-24 00:22:02 +0200

    added root privs check back into install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
+if [ "$(id -u)" != "0" ]; then
+  echo '[Error]: You must run this setup script with root privileges.'
+  echo
+  exit 1
+fi
 sudo apt-get install python3-pip
 sudo apt-get install mono-complete
 sudo pip3 install -e .
